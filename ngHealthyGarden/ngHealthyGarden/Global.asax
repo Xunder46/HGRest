7cd7ff7961ec7dac3553abc1183bb117c6af009b
Xunder46 <ezhix46@gmail.com>
2020-02-18 20:02:01 -0500

    Created MVCapp, AngularApp, Scaffolded DB, Created DbContext

---
+++ ngHealthyGarden/ngHealthyGarden/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ngHealthyGarden.MvcApplication" Language="C#" %>
---
+++ ngHealthyGarden/ngHealthyGarden/PablosDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+namespace ngHealthyGarden
+{
+    public partial class PablosDbContext : DbContext
+    {
+        public PablosDbContext()
+            : base("name=MyEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Dish> Dishes { get; set; }
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<Side> Sides { get; set; }
+        public virtual DbSet<Size> Sizes { get; set; }
+        public virtual DbSet<TortillaType> TortillaTypes { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<DishItem> DishItems { get; set; }
+    }
+}
